7be903aef0b3a1d5c8274729ed5bffed6a7e43cc
mahmoudselem912 <mahmoudselem912@github.com>
2021-07-13 15:49:52 +0200

    change layout

--- src/movies/movies.js
+++ src/movies/movies.js
@@ -1,11 +1,8 @@
 import React from "react";
 import axios from "axios";
 import "./movies.css";
-import "antd/dist/antd.css";
-import { Input } from "antd";
 
 const Movies = () => {
-  const { Search } = Input;
   const apiKey = "7c519f5b71a8cf06004cc91cd32f141f";
 
   React.useEffect(() => {
@@ -29,8 +26,8 @@ const Movies = () => {
         <input type="search" className="search" placeholder="search ..." />
       </div>
 
-      <div>
-        <h2>Popular movies</h2>
+      <div className="popular-movies">
+        <h2 className="popular">Popular movies</h2>
       </div>
     </div>
   );
